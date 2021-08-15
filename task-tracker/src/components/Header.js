6f2bdfc454b58e60f8c750adfd9dd26b87f2b6a5
Junaid Ahmed <junaid@donesol.co.uk>
2021-08-15 07:57:40 +0500

    defaultProps, propTypes in react

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,9 +1,23 @@
+import PropTypes from "prop-types";
+
 const Header = (props) => {
   return (
     <header>
       <h1>{props.title}</h1>
     </header>
   );
+};
+
+//defaultProps are used when below properties are not passed as props
+//e.g, if we not pass title as props then this value is displayed.
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+//To check the type of Prop
+//if we pass title value other than string then it shows Warning!
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
